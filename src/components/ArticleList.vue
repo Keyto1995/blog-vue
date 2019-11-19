a9f78ffe46e96ac8bc6dffb627ac789596a731fa
keyto1995 <keyto1995@outlook.com>
2019-11-19 21:48:18 +0800

    实现目录

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ArticleListItem :article="article" />
+  </div>
+</template>
+
+<script>
+import ArticleListItem from "@/components/ArticleListItem.vue";
+export default {
+  name: "ArticleList",
+  components: {
+    ArticleListItem,
+  },
+  data() {
+    return {
+      article: {
+        id: 12,
+        title: "title",
+        description: "aescription",
+        tags: [
+          {name: "name"},
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container mx-auto">
+      <ArticleList/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import ArticleList from "@/components/ArticleList";
 export default {
   name: "home",
   components: {
-    HelloWorld,
+    ArticleList,
   },
 };
 </script>
