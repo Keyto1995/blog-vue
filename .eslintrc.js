fa8ff5a705680119c51b97aaf6c072f9cbeb19f6
keyto1995 <keyto1995@outlook.com>
2019-11-19 16:23:30 +0800

    eslint添加尾随逗号规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["plugin:vue/essential", 'eslint:recommended'],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": ["error", "always-multiline"],
   },
   parserOptions: {
     parser: "babel-eslint"
