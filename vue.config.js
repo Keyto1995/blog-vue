3ab3a3c7424de345ac6da3119b9ac5d52bbfacb6
keyto1995 <keyto1995@outlook.com>
2019-11-21 01:18:13 +0800

    配置自动格式化

--- vue.config.js
+++ vue.config.js
@@ -13,4 +13,14 @@ module.exports = {
       },
     },
   },
+  chainWebpack: config => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .loader("eslint-loader")
+      .tap(options => {
+        options.fix = true;
+        return options;
+      });
+  },
 };
