cf1bdc7b4943f05d210a3812eac91a87fc750e63
keyto1995 <keyto1995@outlook.com>
2019-11-29 22:45:06 +0800

    实现axios全局处理

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
 import axios from "axios";
 
 import {
@@ -37,6 +36,35 @@ Vue.prototype.$message = message;
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "production" ? "https://api.keyto.top/" : "/api";
+axios.interceptors.response.use(
+  res => {
+    return res;
+  },
+  error => {
+    if (error) {
+      // 请求配置发生的错误
+      if (!error.response) {
+        return console.log("Error", error.message);
+      }
+
+      // 获取状态码
+      const status = error.response.status;
+
+      // 提示错误信息
+      Vue.prototype.$message.error(status, 10);
+
+      // 错误状态处理
+      if (status === 401) {
+        router.push("/login");
+      } else if (status === 403) {
+        router.push("/login");
+      } else if (status >= 404 && status < 422) {
+        router.push("/404");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 Vue.prototype.$axios = axios;
 
 new Vue({
